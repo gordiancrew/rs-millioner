58b2ae7b66b28830cf5b42c6085c2225ba2439fd
AndreiPavlov135 <9000andrey@gmail.com>
2023-01-31 12:30:03 +0300

    fix: add sass loader

--- src/components/start.tsx
+++ src/components/start.tsx
@@ -1,23 +1,28 @@
 import { Link } from "react-router-dom";
-import "../styles/start.css";
+import "../styles/start.scss";
 
 function Start() {
-	return (
-		<div>
-			<h1>Start page.</h1>
-			<p>
-				On this page user enters the game using his login and password or
-				registers.
-			</p>
-			<ul>
-				List url:
-				<li>url "/" - start page (this page)</li>
-				<li>url "/home" -<Link to="/home"> home page</Link></li>
-				<li>url "/question" -<Link to="/question"> page of current question</Link></li>
-				<li>url "/progress" - <Link to="/progress">page of progress</Link></li>
-                
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Start page.</h1>
+      <p>
+        On this page user enters the game using his login and password or
+        registers.
+      </p>
+      <ul>
+        List url:
+        <li>url "/" - start page (this page)</li>
+        <li>
+          url "/home" -<Link to="/home"> home page</Link>
+        </li>
+        <li>
+          url "/question" -<Link to="/question"> page of current question</Link>
+        </li>
+        <li>
+          url "/progress" - <Link to="/progress">page of progress</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 export default Start;
