4b3606ece4a13bb3b31405f770c0a24faee54ded
Syarguk <litsvin@gmail.com>
2023-02-01 16:41:14 +0300

    feat: change background home

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,26 +1,21 @@
-import '../styles/home.scss'
+import style from '../styles/home.module.scss';
+import Hexagon from '../utils/hexagon';
 
 function Home() {
 	return (
-		<div className='page'>
-			<h1>Home</h1>
+		<div className={style.page}>
+			{/* <h1>Home</h1>
 			<p>This home-page shows rules of game, your score for previous games,
                 you rating, end other things of managering.
                 Example: 
                 <a href="https://www.mobygames.com/images/shots/l/684797-who-wants-to-be-a-millionaire-junior-windows-screenshot-the.jpg">
                     THIS IMAGE
                 </a>
-            </p>
+            </p> */}
             
-        <div className="wrap-hexagon">
-            <p className='hexa-text'>Установки игры</p>
-        </div>
-        <div className="wrap-hexagon">
-            <p className='hexa-text'>Начало игры</p>
-        </div>
-        <div className="wrap-hexagon">
-            <p className='hexa-text'>Победители</p>
-        </div>
+            <Hexagon title="Установки игры"/>
+            <Hexagon title="Начало игры"/>
+            <Hexagon title="Результаты игры"/>
 		</div>
 	);
 }
