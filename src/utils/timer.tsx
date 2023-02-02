d34927777ecc38655fcde9e9908c99c428bbd1ca
Vladimir Kazantsev <gordiancrew@gmail.com>
2023-02-03 00:12:19 +0300

    fix: component timer

--- src/utils/timer.tsx
+++ src/utils/timer.tsx
@@ -1,21 +1,25 @@
+import { useEffect, useState } from "react";
 import timerStyle from "../styles/timer.module.scss";
+import TimeProgress from "./time-progress";
 
 function Timer() {
-    const arr: any = [];
-    arr.length = 10;
-    return (
+	const [timer, setTimer] = useState(30);
 
-        <div className={timerStyle.timerBox}>
-            <div className={timerStyle.timerProgress}>
+	useEffect(() => {
+		let timeGo = setInterval(() => {
+			setTimer((x) => (x > 0 ? x - 1 : x));
+		}, 1000);
+	}, []);
+	useEffect(() => {
+		if (timer === 0) alert("TIME IS OUT");
+	}, [timer]);
 
-               
-
-
-                
-            </div>
-            <div className={timerStyle.timerCircle}>12</div>
-        </div>
-    );
-
+	return (
+		<div className={timerStyle.timerBox}>
+			<TimeProgress numb={timer} />
+			<div className={timerStyle.timerCircle}>{timer}</div>
+			<TimeProgress numb={timer} />
+		</div>
+	);
 }
 export default Timer;
