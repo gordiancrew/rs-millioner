804bd64edf45fcbcfe1b548ceef3252c20aa4d8d
AndreiPavlov135 <9000andrey@gmail.com>
2023-02-05 02:22:05 +0300

    refactor: add adaptation styles

--- src/utils/question-header.tsx
+++ src/utils/question-header.tsx
@@ -1,32 +1,33 @@
 import headerStyle from "../styles/question-header.module.scss";
-interface IHeaderOptions{
-	setVisibleHintBoolean:Function;
-	setTimeOn:Function;
-	itemHintBoolean:boolean
-	setItemHintBoolean:Function
+interface IHeaderOptions {
+  setVisibleHintBoolean: Function;
+  setTimeOn: Function;
+  itemHintBoolean: boolean;
+  setItemHintBoolean: Function;
 }
-function QuestionHeader(props:IHeaderOptions) {
-	function booleanHundler(){
-		props.setVisibleHintBoolean(true)
-		props.setTimeOn(false)
-	}
-	return (
-		<div className={headerStyle.headerBox}>
-			<div className={headerStyle.headerHints}>
-				<div
-				onClick={!props.itemHintBoolean?booleanHundler:()=>console.log('pp')}
-				style={{backgroundColor:props.itemHintBoolean?'black':''}}
-				
-				
-				className={headerStyle.headerItem}>Boolean</div>
-				<div className={headerStyle.headerItem}></div>
-				<div className={headerStyle.headerItem}></div>
-			</div>
-			<div className={headerStyle.headerHints}>
-				<div className={headerStyle.headerItem}></div>
-				<div className={headerStyle.headerItem}></div>
-			</div>
-		</div>
-	);
+function QuestionHeader(props: IHeaderOptions) {
+  function booleanHundler() {
+    props.setVisibleHintBoolean(true);
+    props.setTimeOn(false);
+  }
+  return (
+    <div className={headerStyle.headerBox}>
+      <div className={headerStyle.headerHints}>
+        <div
+          onClick={!props.itemHintBoolean ? booleanHundler : () => {}}
+          style={{ backgroundColor: props.itemHintBoolean ? "black" : "" }}
+          className={headerStyle.headerItem}
+        >
+          Boolean
+        </div>
+        <div className={headerStyle.headerItem}></div>
+        <div className={headerStyle.headerItem}></div>
+      </div>
+      <div className={headerStyle.headerHints}>
+        <div className={headerStyle.headerItem}></div>
+        <div className={headerStyle.headerItem}></div>
+      </div>
+    </div>
+  );
 }
 export default QuestionHeader;
