468762800c706c84cfdf0017e51071dd36848720
AndreiPavlov135 <9000andrey@gmail.com>
2023-02-08 18:42:21 +0300

    refactor: change styles in form

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,20 +1,19 @@
-import { SignInUp } from './signInUp';
-import '../styles/home.scss'
+import { SignInUp } from "./signInUp";
+import "../styles/home.scss";
 
 function Home() {
-
-	return (
-		<div className='page'>
-			<h1>Home</h1>
+  return (
+    <div className="page">
+      {/* <h1>Home</h1>
 			<p>This home-page shows rules of game, your score for previous games,
                 you rating, end other things of managering.
                 Example: 
                 <a href="https://www.mobygames.com/images/shots/l/684797-who-wants-to-be-a-millionaire-junior-windows-screenshot-the.jpg">
                     THIS IMAGE
                 </a>
-            </p>
-                <SignInUp/>
-		</div>
-	);
+            </p> */}
+      <SignInUp />
+    </div>
+  );
 }
 export default Home;
