7d1db78756c963e5c7ebc0f93ba2cde509e91310
AndreiPavlov135 <9000andrey@gmail.com>
2023-02-20 23:08:28 +0300

    feat: add rules to game

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,29 @@ import Profile from "./components/profile";
 import Question from "./components/question";
 import Start from "./components/start";
 import { useTranslation } from "react-i18next";
-
-
+import Rules from "./utils/rules";
 
 function App() {
   const { t, i18n } = useTranslation();
   const changeLanguage = (language: string) => {
     i18n.changeLanguage(language);
     localStorage.setItem("languagegame", language);
-  }
+  };
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home 
-                                  t={t}
-                                  changeLng={changeLanguage}
-                                 />} />
+        <Route path="/" element={<Home t={t} changeLng={changeLanguage} />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/home" element={<Start 
-                                      changeLng={changeLanguage} 
-                                      t={t}
-                                    />} />
+        <Route
+          path="/home"
+          element={<Start changeLng={changeLanguage} t={t} />}
+        />
 
-        <Route path="/question" element={<Question t={t}/>} />
+        <Route path="/question" element={<Question t={t} />} />
+        <Route path="/rules" element={<Rules t={t} />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
- 
