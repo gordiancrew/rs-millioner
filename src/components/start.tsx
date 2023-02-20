9e87b173059ef14bf183d96bebacf722b0423224
AndreiPavlov135 <9000andrey@gmail.com>
2023-02-20 15:44:18 +0300

    feat: add rules in game

--- src/components/start.tsx
+++ src/components/start.tsx
@@ -9,40 +9,34 @@ interface IStart {
   autorisation: boolean;
 }
 
-function Start({changeLng, t, autorisation}: IStart) {
-  const[login,setLogin]=useState('')
-  function logout(){
-    localStorage.removeItem('currentName')
-    setLogin('1')
+function Start({ changeLng, t, autorisation }: IStart) {
+  const [login, setLogin] = useState("");
+  function logout() {
+    localStorage.removeItem("currentName");
+    setLogin("1");
   }
-  
+
   return (
-    <div className='startWrapper'>
-       {
-        localStorage.currentName ? (
-         <div className="autorName">{"Вы авторизированы как \""+localStorage.currentName+"\""}</div>
-        ) : null
-      }
+    <div className="startWrapper">
+      {localStorage.currentName ? (
+        <div className="autorName">
+          {'Вы авторизированы как "' + localStorage.currentName + '"'}
+        </div>
+      ) : null}
       <LangButtons changeLng={changeLng} />
-      {
-        localStorage.currentName ? (
-          <HexagonButton content={t("signinup.yourprofile")} link="/profile" />
-        ) : null
-      }
-       {
-        localStorage.currentName ? (
-          <div onClick={logout}>
-          <HexagonButton content={"Разлогинится"} link="/home" />
-          </div>
-        ) : null
-      }
-       {
-        !localStorage.currentName? (
-          <HexagonButton content={"авторизироваться"} link="/" />
-        ) : null
-      }
+      {localStorage.currentName ? (
+        <HexagonButton content={t("signinup.yourprofile")} link="/profile" />
+      ) : null}
+      {!localStorage.currentName ? (
+        <HexagonButton content={t("signinup.autoris")} link="/" />
+      ) : null}
       <HexagonButton content={t("signinup.play")} link="/question" />
-      <HexagonButton content={t("signinup.rules")} link="/home" />
+      <HexagonButton content={t("signinup.rules")} link="/rules" />
+      {localStorage.currentName ? (
+        <div onClick={logout} style={{ display: "contents" }}>
+          <HexagonButton content={t("signinup.logOut")} link="/home" />
+        </div>
+      ) : null}
     </div>
   );
 }
